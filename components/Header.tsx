9795cb00a9346353735c7558d47baae4c7f9f48f
David Sosby <dsosby@gmail.com>
2023-08-29 16:58:04 -0700

    Style tune

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,12 @@ import Rays from "./Rays.tsx";
 
 export default function Header() {
   return (
-    <div class="relative max-w-screen h-96 bg-gradient-to-r from-carolinaBlue to-blue">
+    <div class="relative max-w-screen h-96 bg-gradient-to-r from-carolinaBlue to-blue border-b-1 border-white border-solid">
       <Rays />
       <div class="relative flex h-full flex-col items-center justify-center">
-        <h1 class="text-4xl font-normal tracking-wide">Hi, I'm <span class="name">David!</span></h1>
+        <h1 class="text-5xl font-normal tracking-wide">Hi, I'm <span class="name">David!</span> 👋🏻</h1>
         <h2 class="text-2xl font-light tracking-wide">
-          (but friends call me <span title="saws-bee" class="pronounce">Sosby</span>)
+          but friends just call me <span title="saws-bee" class="pronounce">sosby</span>
         </h2>
       </div>
     </div>
