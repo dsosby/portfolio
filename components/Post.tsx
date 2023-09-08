fc0bf992d11ed0e5fa0b604b814f61d3b3937fa3
David Sosby <dsosby@gmail.com>
2023-09-07 17:20:16 -0700

    Big screen readability

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,10 @@
 import { render } from "$gfm";
 import { Post } from "@/data/posts.ts";
 
+import "https://esm.sh/prismjs@1.29.0/components/prism-clojure?no-check";
+import "https://esm.sh/prismjs@1.29.0/components/prism-java?no-check";
+import "https://esm.sh/prismjs@1.29.0/components/prism-scheme?no-check";
+
 export type PostProps = {
   readonly post: Post;
 }
@@ -10,7 +14,7 @@ export default function Post({ post }: PostProps) {
   const link = `/posts/${post.slug}`;
 
   return (
-    <div class="flex flex-col mb-5">
+    <div class="flex flex-col mb-5 max-w-[68rem]">
       <h1 class="text-2xl font-black">{post.title}</h1>
       <div class="markdown-body" data-color-mode="dark" data-dark-theme="dark" dangerouslySetInnerHTML={{ __html: renderedPost }} />
     </div>
