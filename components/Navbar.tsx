96e287e7d2b120c9a1bc2d5a6a7fd0f3990c304d
David Sosby <dsosby@gmail.com>
2023-09-17 23:04:47 -0700

    Updated navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,32 +8,30 @@ export default function Navbar() {
         <a href="/">
           <Logo />
         </a>
-        <div class="flex flex-row items-center gap-3">
-          <a href="/articles">
-            <span class="font-light">Articles</span>
-          </a>
-          <a href="/career">
-            <span class="font-light">Career</span>
-          </a>
-          <a href="/about">
-            <span class="font-light">About</span>
-          </a>
-        </div>
       </div>
-      <div class="basis-0 flex flex-row gap-3 mx-2">
+      <div class="basis-0 flex flex-row flex-wrap gap-3 mx-2 items-center">
+        <a href="/articles">
+          <span class="font-light">Articles</span>
+        </a>
+        {/* <a href="/career">
+          <span class="font-light">Career</span>
+        </a> */}
+        <a href="/about">
+          <span class="font-light">About</span>
+        </a>
         <a
           href="https://github.com/dsosby"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <IconBrandGithub class="w-6 h-6" />
+          <IconBrandGithub aria-label="Gihub" class="w-5 h-5" />
         </a>
         <a
           href="https://www.linkedin.com/in/david-sosby-b30055b/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <IconBrandLinkedin class="w-6 h-6" />
+          <IconBrandLinkedin aria-label="LinkedIn" class="w-5 h-5" />
         </a>
       </div>
     </nav>
